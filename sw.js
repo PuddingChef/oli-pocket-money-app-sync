7d939c57c177ccb4b990f15cc9837bbec6f48f79
PuddingChef <87723091+PuddingChef@users.noreply.github.com>
2026-01-21 00:46:25 +0000

    Update sw.js
    cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "olis-pocket-money-v8";
+const CACHE_NAME = "olis-pocket-money-v9";
 const ASSETS = [
   "./",
   "./index.html",
@@ -46,6 +46,7 @@ self.addEventListener("message", (event) => {
     }).catch(()=>{});
   }
 });
+
 
 
 
